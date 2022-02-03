7e9ec3b1aed397bf33409b69bdc4112331110d2a
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-02-03 16:50:26 +0100

    ✨feat(package.json): ajout de node server.js

---
+++ react-news/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import News from './pages/News';
+
+const App = () => {
+  return (
+    <div className="App">
+      <News />
+    </div>
+  );
+};
+
+export default App;
